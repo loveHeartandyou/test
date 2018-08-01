22b4c3ed74cc5d3890e29e70f575c4ed8591a1a3
apple <>
2018-08-01 17:47:32 +0800

    first commit

--- TestGit/TestGit/main.m
+++ TestGit/TestGit/main.m
@@ -11,6 +11,7 @@
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        NSLog(@"hellow world!");
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
